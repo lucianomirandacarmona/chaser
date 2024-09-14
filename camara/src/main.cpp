c1ae275604b053df094c90b7efa3d9d67835a793
Mario Medina <mario@mariomedina.net>
2024-09-14 14:46:00 -0500

    cambiando el formato a arduino

--- camara/src/main.cpp
+++ camara/src/main.cpp
@@ -1,5 +1,14 @@
+#include <Arduino.h>
 #include "esp_camera.h"
 #include <WiFi.h>
+#include <BluetoothSerial.h>
+#include <Preferences.h>
+Preferences prefs;
+BluetoothSerial SerialBT;
+
+#define BT_DISCOVER_TIME  10000
+esp_spp_sec_t sec_mask=ESP_SPP_SEC_NONE; // or ESP_SPP_SEC_ENCRYPT|ESP_SPP_SEC_AUTHENTICATE to request pincode confirmation
+esp_spp_role_t role=ESP_SPP_ROLE_SLAVE; // or ESP_SPP_ROLE_MASTER
 
 //
 // WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
@@ -41,10 +50,65 @@ const char* password = "p73G4HBe8c";
 void startCameraServer();
 void setupLedFlash(int pin);
 
+void HandleBluetooth(void *params) {
+
+  if(! SerialBT.begin("esp32cam", true) ) {
+    Serial.println("========== serialBT failed!");
+    abort();
+  }
+
+    Serial.println("Starting discoverAsync...");
+  BTScanResults* btDeviceList = SerialBT.getScanResults();  // maybe accessing from different threads!
+  if (SerialBT.discoverAsync([](BTAdvertisedDevice* pDevice) {
+      // BTAdvertisedDeviceSet*set = reinterpret_cast<BTAdvertisedDeviceSet*>(pDevice);
+      // btDeviceList[pDevice->getAddress()] = * set;
+      Serial.printf(">>>>>>>>>>>Found a new device asynchronously: %s\n", pDevice->toString().c_str());
+    } )
+    ) {
+    delay(BT_DISCOVER_TIME);
+    Serial.print("Stopping discoverAsync... ");
+    SerialBT.discoverAsyncStop();
+    Serial.println("discoverAsync stopped");
+    delay(5000);
+    if(btDeviceList->getCount() > 0) {
+      BTAddress addr;
+      int channel=0;
+      Serial.println("Found devices:");
+      for (int i=0; i < btDeviceList->getCount(); i++) {
+        BTAdvertisedDevice *device=btDeviceList->getDevice(i);
+        Serial.printf(" ----- %s  %s %d\n", device->getAddress().toString().c_str(), device->getName().c_str(), device->getRSSI());
+        std::map<int,std::string> channels=SerialBT.getChannels(device->getAddress());
+        Serial.printf("scanned for services, found %d\n", channels.size());
+        for(auto const &entry : channels) {
+          Serial.printf("     channel %d (%s)\n", entry.first, entry.second.c_str());
+        }
+        if(channels.size() > 0) {
+          addr = device->getAddress();
+          channel=channels.begin()->first;
+        }
+      }
+      if(addr) {
+        Serial.printf("connecting to %s - %d\n", addr.toString().c_str(), channel);
+        SerialBT.connect(addr, channel, sec_mask, role);
+      }
+    } else {
+      Serial.println("Didn't find any devices");
+    }
+  } else {
+    Serial.println("Error on discoverAsync f.e. not workin after a \"connect\"");
+  }
+
+}
 void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
   Serial.println();
+
+  prefs.begin("camara");
+  prefs.getString("SSID");
+  prefs.getString("PASS");
+
+  HandleBluetooth(NULL);
 
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
